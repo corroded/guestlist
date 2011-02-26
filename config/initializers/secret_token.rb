47d593b30ac10a68752b25b8cdc585f5f974ecd0
Eumir Gaspar <eumir@aelogica.com>
2011-02-27 00:29:51 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestlist::Application.config.secret_token = '9692ee433426aa3650a2b8c2e3150818c2bb36ea77404278525ebbf981c30394a18a706f73679c26a72c7dd9be8f26403c3f89aad06ea551e42f8c2e74b9d52f'
